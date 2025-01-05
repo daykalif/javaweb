e3a960b60bbfd44ac6c455f0ddc8f9e0987d9698
daykalif <1038200595@qq.com>
2025-01-05 11:08:20 +0800

    feat:36 单元测试-断言

--- javaweb-ai-project01/maven-project01/src/test/java/com/itheima/UserServiceTest.java
+++ javaweb-ai-project01/maven-project01/src/test/java/com/itheima/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.itheima;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /*
@@ -15,6 +16,16 @@ import org.junit.jupiter.api.Test;
  * 	3.JUnit单元测试的命名规范？
  * 		类：XxxxTest							（规范：只是建议，不是强制的）
  * 		方法：public void xxxx(){...}		 (规定：强制的，必需按照这个格式使用)
+ *
+ * 思考：
+ * 	1.单元测试运行不报错（绿色），就代表代码没问题，测试通过？
+ * 		- 并不是
+ *
+ * 小结：
+ * 	1.在JUnit单元测试中，为什么要使用断言？
+ * 		- 单元测试方法运行不报错，不代表业务方法没问题。
+ * 		- 通过断言可以检测方法运行结果是否和预期一致，从而判断业务方法的正确性。
+ * 		- Assertion.assertXxxx(...)
  */
 public class UserServiceTest {
 	@Test
@@ -22,5 +33,39 @@ public class UserServiceTest {
 		UserService userService = new UserService();
 		Integer age = userService.getAge("330109199906054110");// 身份证
 		System.out.println(age);
+	}
+
+	@Test
+	public void testGetGender() {
+		UserService userService = new UserService();
+		String gender = userService.getGender("330109199906054110");
+		System.out.println(gender);
+	}
+
+	/**
+	 * 断言正常情况
+	 */
+	@Test
+	public void testGenderWithAssert() {
+		UserService userService = new UserService();
+		String gender = userService.getGender("330109199906054110");
+		Assertions.assertEquals("男", gender);
+		Assertions.assertEquals("女", gender, "性别实际值与期望不符");
+	}
+
+	/**
+	 * 断言异常情况
+	 */
+	@Test
+	public void testGenderWithAssert2() {
+		UserService userService = new UserService();
+		String gender = userService.getGender("330109199906054110");
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			userService.getGender(null);
+		});
+
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			userService.getGender(null);
+		});
 	}
 }
