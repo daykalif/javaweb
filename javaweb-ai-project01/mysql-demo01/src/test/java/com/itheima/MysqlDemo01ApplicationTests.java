d3461981077b5d07252b6c764bd4c79d2c2e55c0
daykalif <1038200595@qq.com>
2025-01-10 23:13:31 +0800

    feat:51 MySQL-概述-安装&数据模型

---
+++ javaweb-ai-project01/mysql-demo01/src/test/java/com/itheima/MysqlDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- javaweb-ai-project01/readme.md
+++ javaweb-ai-project01/readme.md
@@ -1,2 +1,10 @@
 # 课程讲义
 [讲义地址](https://heuqqdmbyk.feishu.cn/wiki/LYVswfK4eigRIhkW0pvcqgH9nWd)
+
+终端启动mysql：`mysql -u root -p`
+
+输入password:`Wjp...`
+
+查看数据库：`show databases;`
+
+使用某一个database，如：`use Daykalif_project_login`
