8402a65f71e05e0d8ef91a388e0885644184e70e
daykalif <1038200595@qq.com>
2025-01-11 06:27:38 +0800

    feat:52 MySQL-SQL-DDL-数据库操作

---
+++ javaweb-ai-project01/mysql-demo01/src/main/java/com/itheima/Test.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+/*
+ * DDL关于数据库的操作小结：
+ * 	1.同一个数据库服务器中，数据库的名字是否可以相同？
+ * 		- 不可以
+ * 	2.MySQL8版本默认的字符集是什么？
+ * 		- utf8mb4
+ * 		- default_charset utf8mb4
+ */
+public class Test {
+}
