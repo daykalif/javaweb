ee35e2443ba03337a50be86383c1d04c8d9a9a8e
daykalif <1038200595@qq.com>
2025-01-11 10:13:10 +0800

    feat:53 MySQL-SQL-DDL-表操作-创建表

--- javaweb-ai-project01/mysql-demo01/src/main/java/com/itheima/Test.java
+++ javaweb-ai-project01/mysql-demo01/src/main/java/com/itheima/Test.java
@@ -9,4 +9,42 @@ package com.itheima;
  * 		- default_charset utf8mb4
  */
 public class Test {
+/**
+ *-- 查询数据库
+ * show databases;
+ *
+ * -- 创建数据库
+ * create database db01;
+ *
+ * -- 切换数据库
+ * use db01;
+ *
+ * -- 查询当前正在使用的数据库
+ * select database();
+ *
+ * -- 删除数据库
+ * drop database db01;
+ *
+ * -- ------------> DDL 表操作----------------
+ * -- 创建表
+ * create table user
+ * (
+ *     id       int comment 'ID，唯一标识',
+ *     username varchar(50) comment '用户名',
+ *     name     varchar(10) comment '姓名',
+ *     age      int comment '年龄',
+ *     gender   char(1) comment '性别'
+ * ) comment '用户信息表';
+ *
+ *
+ * -- 创建表（约束）
+ * create table user
+ * (
+ *     id       int primary key auto_increment comment 'ID，唯一标识', -- 主键约束 auto_increment
+ *     username varchar(50) not null unique comment '用户名',         -- 非空，唯一
+ *     name     varchar(10) not null comment '姓名',                  -- 非空
+ *     age      int comment '年龄',
+ *     gender   char(1) default '男' comment '性别'                   -- 默认
+ * ) comment '用户信息表';
+ */
 }
