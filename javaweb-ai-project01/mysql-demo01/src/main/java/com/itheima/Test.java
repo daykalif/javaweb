a945bdd5ef0ce4bb4364bddec48997234e728e30
daykalif <1038200595@qq.com>
2025-01-12 12:11:47 +0800

    feat:61 MySQL-SQL-DQL-升序查询

--- javaweb-ai-project01/mysql-demo01/src/main/java/com/itheima/Test.java
+++ javaweb-ai-project01/mysql-demo01/src/main/java/com/itheima/Test.java
@@ -407,5 +407,33 @@ public class Test {
 	 * where entry_date <= '2015-01-01'
 	 * group by job
 	 * having count(*) >= 2;
+	 *
+	 *
+	 *
+	 * --  =================== 排序查询 ======================
+	 * -- 1. 根据入职时间, 对员工进行升序排序
+	 * select *
+	 * from emp
+	 * order by entry_date;
+	 *
+	 * select *
+	 * from emp
+	 * order by entry_date asc;
+	 *
+	 *
+	 * -- 2. 根据入职时间, 对员工进行降序排序
+	 * select *
+	 * from emp
+	 * order by entry_date desc;
+	 *
+	 *
+	 * -- 3. 根据 入职时间 对公司的员工进行 升序排序 ， 入职时间相同 , 再按照 更新时间 进行降序排序
+	 * select *
+	 * from emp
+	 * order by entry_date, update_time desc;
+	 *
+	 * select *
+	 * from emp
+	 * order by entry_date asc, update_time desc;
 	 */
 }
