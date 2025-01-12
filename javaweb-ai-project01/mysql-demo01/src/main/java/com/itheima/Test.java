61b84ac805320545fcaf6cf17cdb387449fc3728
daykalif <1038200595@qq.com>
2025-01-12 12:22:59 +0800

    feat:61 MySQL-SQL-DQL-分页查询

--- javaweb-ai-project01/mysql-demo01/src/main/java/com/itheima/Test.java
+++ javaweb-ai-project01/mysql-demo01/src/main/java/com/itheima/Test.java
@@ -435,5 +435,33 @@ public class Test {
 	 * select *
 	 * from emp
 	 * order by entry_date asc, update_time desc;
+	 *
+	 *
+	 *
+	 * --  =================== 分页查询 ======================
+	 * -- 1. 从起始索引0开始查询员工数据, 每页展示5条记录
+	 * select *
+	 * from emp
+	 * limit 0,5;
+	 *
+	 * -- 2. 查询 第1页 员工数据, 每页展示5条记录
+	 * select *
+	 * from emp
+	 * limit 0,5;
+	 *
+	 * -- 3. 查询 第2页 员工数据, 每页展示5条记录
+	 * select *
+	 * from emp
+	 * limit 5,5;
+	 *
+	 * -- 4. 查询 第3页 员工数据, 每页展示5条记录
+	 * select *
+	 * from emp
+	 * limit 10,5;
+	 *
+	 * -- 4. 查询 第 pageNumber 页 员工数据, 每页展示 pageSize 条记录
+	 * -- 页码
+	 * -- 起始索引 =（页码 - 1）* 每页展示记录数
+	 * # select * from emp limit (pageNumber-1) * pageSize,pageSize;
 	 */
 }
