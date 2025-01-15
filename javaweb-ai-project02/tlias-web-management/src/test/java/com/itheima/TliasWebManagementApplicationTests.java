8a366d103880d0b3820074389dcac3a3d78a467a
daykalif <1038200595@qq.com>
2025-01-15 15:44:06 +0800

    feat:76 准备工作-工程搭建-测试是否运行正常

--- javaweb-ai-project02/tlias-web-management/src/test/java/com/itheima/TliasWebManagementApplicationTests.java
+++ javaweb-ai-project02/tlias-web-management/src/test/java/com/itheima/TliasWebManagementApplicationTests.java
@@ -1,13 +1,22 @@
 package com.itheima;
 
+import com.itheima.mapper.DeptMapper;
+import com.itheima.pojo.Dept;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class TliasWebManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired    // 依赖注入的注解
+	private DeptMapper deptMapper;
 
+	@Test
+	public void testFindAll() {
+		List<Dept> deptList = deptMapper.findAll();
+		deptList.forEach(System.out::println);
+	}
 }
