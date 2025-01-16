f6bc90e7abba224872873034972348a5557c5a3d
daykalif <1038200595@qq.com>
2025-01-16 15:19:24 +0800

    feat:78 部门管理-列表查询-结果封装

--- javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
+++ javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
@@ -2,6 +2,8 @@ package com.itheima.mapper;
 
 import com.itheima.pojo.Dept;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -11,6 +13,22 @@ public interface DeptMapper {
 	/*
 	 * 查询所有部门
 	 */
+
+	/*
+	 * 方式一：手动结果映射
+	 *
+	 * @Results({
+	 *        @Result(column = "create_time", property = "createTime"),
+	 *        @Result(column = "update_time", property = "updateTime")
+	 * })
+	 */
+
+	/*
+	 * 方式二：起别名
+	 *
+	 * @Select("select id, name, create_time createTime, update_time updateTime from dept order by update_time desc;")
+	 */
+
 	// 选中括号中的内容，右键>"Show Context Actions">"inject language or reference">MySql
 	@Select("select id, name, create_time, update_time from dept order by update_time desc;")
 	public List<Dept> findAll();
