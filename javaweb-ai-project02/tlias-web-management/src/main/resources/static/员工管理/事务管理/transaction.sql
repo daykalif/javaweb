bcf4fc7834691ec142ee4d33a07ba1ab25ad4b78
daykalif <1038200595@qq.com>
2025-01-26 09:55:59 +0800

    feat:101 事务管理-介绍与操作

---
+++ javaweb-ai-project02/tlias-web-management/src/main/resources/static/员工管理/事务管理/transaction.sql
@@ -0,0 +1,19 @@
+-- 开启事务
+start transaction;
+
+-- 保存员工基本信息 - emp
+insert into emp (username, name, gender, phone, job, salary, image, entry_date, dept_id, create_time, update_time)
+values ('lingpingzhi', '林平之', 1, '18809091213', 1, 6000,
+        'https://web-framework.oss-cn-hangzhou.aliyuncs.com/2023/1.jpg', '2020-01-01', 1, '2024-10-01 16:35:33',
+        '2024-10-01 16:36:15');
+
+-- 批量保存员工工作经历信息 - emp_expr
+insert into emp_expr (emp_id, begin, end, company, job)
+values (37, '2020-01-01', '2021-01-01', '百度', 'java开发'),
+       (37, '2021-01-01', '2023-01-01', '字节', 'java开发');
+
+-- 提交事务（提交事务 与 回滚事务 互斥）
+commit;
+
+-- 回滚事务
+rollback;
