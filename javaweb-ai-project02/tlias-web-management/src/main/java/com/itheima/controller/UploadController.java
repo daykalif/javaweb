2e57ac1962e82f7793e40d291d4e83681bbc0cb1
daykalif <1038200595@qq.com>
2025-01-26 15:53:51 +0800

    feat:105 文件上传-本地存储

--- javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
+++ javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
@@ -6,16 +6,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 public class UploadController {
 
 	/**
 	 * 本地磁盘存储方案
+	 *
+	 * springboot默认最大上传文件大小为1MB，可通过配置文件修改
 	 */
 	@PostMapping("/upload")
-	public Result upload(String name, Integer age, MultipartFile file) {
+	public Result upload(String name, Integer age, MultipartFile file) throws IOException {
 		log.info("接收参数: {},{},{}", name, age, file);
+
+		//获取原始文件名
+		String originalFilename = file.getOriginalFilename();
+
+		//新的文件名
+		String extension = originalFilename.substring(originalFilename.lastIndexOf("."));	// 获取文件后缀名
+		String newFileName = UUID.randomUUID().toString() + extension;	// 生成新的文件名（uuid + 文件后缀名）
+
+		//保存文件
+		file.transferTo(new File("/Users/daykalif/Desktop/web-project/javaweb-ai-project02/tlias-web-management/CustomLog/images/" + newFileName));
 		return Result.success();
 	}
 }
