cc676df579c588aa802fca34c90d5fee08492739
daykalif <1038200595@qq.com>
2025-01-29 13:59:13 +0800

    feat：127 登录认证-登录校验-Filter-入门

--- javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
+++ javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
@@ -2,7 +2,9 @@ package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 开启了SpringBoot对Servlet组件的支持
 @SpringBootApplication    // 默认扫描当前包（该项目中指的是com.itheima这个包）及其子包
 public class TliasWebManagementApplication {
 
