a3f9a1b2fa19636a6be3be754575764fb259451f
daykalif <1038200595@qq.com>
2025-01-29 21:51:33 +0800

    feat：131 登录认证-登录校验-Interceptor-详解

--- javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/config/WebConfig.java
+++ javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/config/WebConfig.java
@@ -16,15 +16,15 @@ public class WebConfig implements WebMvcConfigurer {
 	@Autowired
 	private DemoInterceptor demoInterceptor;
 
-	//@Autowired
-	//private TokenInterceptor tokenInterceptor;
+	@Autowired
+	private TokenInterceptor tokenInterceptor;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(demoInterceptor).addPathPatterns("/**");    // 拦截所有请求
 
-		//registry.addInterceptor(tokenInterceptor)
-		//		.addPathPatterns("/**") // 拦截所有请求
-		//		.excludePathPatterns("/login"); // 不拦截哪些请求
+		registry.addInterceptor(tokenInterceptor)
+				.addPathPatterns("/**") // 拦截所有请求
+				.excludePathPatterns("/login"); // 不拦截哪些请求
 	}
 }
