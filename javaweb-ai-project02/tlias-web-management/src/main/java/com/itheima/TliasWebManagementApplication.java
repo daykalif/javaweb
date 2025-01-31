9bf3dcb40996b35154cabef71e7944b4791c157f
daykalif <1038200595@qq.com>
2025-01-31 17:53:19 +0800

    feat：142 原理篇-Bean管理-第三方bean

--- javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
+++ javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
@@ -1,8 +1,12 @@
 package com.itheima;
 
+import com.itheima.utils.AliyunOSSThirdOperator;
+import com.itheima.utils.AliyunOSSProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan // 开启了SpringBoot对Servlet组件的支持
 @SpringBootApplication    // 默认扫描当前包（该项目中指的是com.itheima这个包）及其子包
@@ -12,4 +16,11 @@ public class TliasWebManagementApplication {
 		SpringApplication.run(TliasWebManagementApplication.class, args);
 	}
 
+	/**
+	 * 方式一：启动类中声明第三方bean
+	 */
+	@Bean
+	public AliyunOSSThirdOperator getAliyunOSSOperatorThird(@Autowired AliyunOSSProperties aliyunOSSProperties) {
+		return new AliyunOSSThirdOperator(aliyunOSSProperties);
+	}
 }
--- javaweb-ai-project02/tlias-web-management/src/test/java/com/itheima/SpringbootWebTests.java
+++ javaweb-ai-project02/tlias-web-management/src/test/java/com/itheima/SpringbootWebTests.java
@@ -1,14 +1,22 @@
 package com.itheima;
 
+import cn.hutool.core.io.FileUtil;
+import com.itheima.utils.AliyunOSSThirdOperator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
+import java.io.File;
+import java.nio.file.Files;
+
 @SpringBootTest
 public class SpringbootWebTests {
 	@Autowired
 	private ApplicationContext applicationContext;
+
+	@Autowired
+	private AliyunOSSThirdOperator aliyunOSSThirdOperator;
 
 	@Test
 	public void testScope() {
@@ -16,5 +24,12 @@ public class SpringbootWebTests {
 			Object deptController = applicationContext.getBean("deptController");
 			System.out.println(deptController);
 		}
+	}
+
+	@Test
+	public void testAliyunOSSOperatorThird() throws Exception {
+		File file = new File("/Users/daykalif/Desktop/web-project/javaweb-ai-project02/tlias-web-management/src/main/resources/static/原理篇/Bean管理/4.第三方bean.png");
+		String url = aliyunOSSThirdOperator.uploadFile(FileUtil.readBytes(file), "4.第三方bean.png");
+		System.out.println(url);
 	}
 }
