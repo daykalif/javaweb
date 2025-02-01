117264333d8b6a8185bf4bd264e9297e665bef9a
daykalif <1038200595@qq.com>
2025-02-01 10:14:21 +0800

    feat：144 原理篇-SpringBoot原理-自动配置-方案一：@ComponentScan

--- javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
+++ javaweb-ai-project02/tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
@@ -7,9 +7,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+/*
+ * 自动配置实现方案一：@ComponentScan
+ * 		扫描指定包下的所有组件, 包括第三方的组件，如果定义了@ComponentScan，@SpringBootApplication扫描会失效
+ */
+@ComponentScan(basePackages = {"com.itheima", "com.example"})
 @ServletComponentScan // 开启了SpringBoot对Servlet组件的支持
-@SpringBootApplication    // 默认扫描当前包（该项目中指的是com.itheima这个包）及其子包
+@SpringBootApplication    // 具备组件扫描功能，默认扫描当前包（该项目中指的是com.itheima这个包）及其子包
 public class TliasWebManagementApplication {
 
 	public static void main(String[] args) {
---
+++ javaweb-ai-project02/tlias-web-management/src/test/java/com/itheima/GsonTest.java
@@ -0,0 +1,28 @@
+package com.itheima;
+
+import com.example.TokenParser;
+import com.google.gson.Gson;
+import com.itheima.pojo.Result;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GsonTest {
+	@Autowired
+	private Gson gson;    // SpringBoot自动配置
+
+	@Test
+	public void testJson() {
+		System.out.println(gson.toJson(Result.success("Hello Gson")));
+	}
+
+
+	@Autowired
+	private TokenParser tokenParser;
+
+	@Test
+	public void testTokenParser() {
+		tokenParser.parse();
+	}
+}
