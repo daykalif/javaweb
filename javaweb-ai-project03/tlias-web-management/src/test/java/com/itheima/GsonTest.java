b312c10dd4b2f863cbf969c531195d1593ec7198
daykalif <1038200595@qq.com>
2025-02-01 19:59:10 +0800

    feat：149 Maven高级-继承

--- javaweb-ai-project03/tlias-web-management/src/test/java/com/itheima/GsonTest.java
+++ javaweb-ai-project03/tlias-web-management/src/test/java/com/itheima/GsonTest.java
@@ -1,8 +1,5 @@
 package com.itheima;
 
-import com.example.HeaderGenerator;
-import com.example.HeaderParser;
-import com.example.TokenParser;
 import com.google.gson.Gson;
 import com.itheima.pojo.Result;
 import org.junit.jupiter.api.Test;
@@ -18,29 +15,4 @@ public class GsonTest {
 	public void testJson() {
 		System.out.println(gson.toJson(Result.success("Hello Gson")));
 	}
-
-
-	//@Autowired
-	//private TokenParser tokenParser;
-	//
-	//@Test
-	//public void testTokenParser() {
-	//	tokenParser.parse();
-	//}
-
-	@Autowired
-	private HeaderParser headerParser;
-
-	@Test
-	public void testHeaderParser() {
-		headerParser.parse();
-	}
-
-	//@Autowired
-	//private HeaderGenerator headerGenerator;
-	//
-	//@Test
-	//public void testHeaderGenerator() {
-	//	headerGenerator.generate();
-	//}
 }
